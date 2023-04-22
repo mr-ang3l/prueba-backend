6bb4d0f5b3ddf94d296c3ca025c5fa26601cfb38
chrisnaibaf <chris.naibaf@gmail.com>
2023-04-22 09:35:15 -0600

    Update projects settings

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,19 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%$)!=q!h0v8o5s=uw%&y#)9a5ws#%2)iw^(5c49jktv%nw9b@t"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
@@ -38,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
---
+++ backend/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+SECRET_KEY = "development"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []

e3ab3b4a4b80b7f5823246fe1683e31b35348f14
chrisnaibaf <chris.naibaf@gmail.com>
2023-04-22 15:43:38 -0600

    Create ordering algorithm for age and last name

--- usuariosAPI/views.py
+++ usuariosAPI/views.py
@@ -5,22 +5,35 @@ from .serializers import UsuarioSerializer
 from .models import Usuario
 
 
+def algoritmo_ordenamiento(arr, query):
+    lenght = len(arr)
+
+    for i in range(lenght):
+        for j in range(0, lenght - i - 1):
+            if getattr(arr[j], query) > getattr(arr[j + 1], query):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
 class UsuarioView(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
     def list(self, request):
         query = request.GET.get("query", None)
+        queryset = Usuario.objects.all()
 
-        if query == "edad":
-            return Response({"mensaje": "Orden por edad"}, status=status.HTTP_200_OK)
-
-        if query == "apellido":
+        if query != None:
             return Response(
-                {"mensaje": "Orden por apellido"}, status=status.HTTP_200_OK
+                self.serializer_class(
+                    algoritmo_ordenamiento(list(queryset), query), many=True
+                ).data,
+                status=status.HTTP_200_OK,
             )
 
-        return Response(
-            self.serializer_class(self.queryset, many=True).data,
-            status=status.HTTP_200_OK,
-        )
+        else:
+            return Response(
+                self.serializer_class(queryset, many=True).data,
+                status=status.HTTP_200_OK,
+            )

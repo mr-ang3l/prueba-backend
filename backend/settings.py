415ce126273e17e1e786614724623c2c651549a7
chrisnaibaf <chris.naibaf@gmail.com>
2023-05-13 16:29:54 -0600

    Prueba key decouple

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,9 +34,9 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
-SECRET_KEY = "development"
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", ".vercel.app"]
 
